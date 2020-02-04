74a36cc48a2556b95e458c8b086ad68bcc2ca922
David Chaves <dchaves@gradiant.org>
2020-02-04 16:25:59 +0100

    Reorganizacion de codigo. Tareas asignadas a core 1 y 2 de la CPU

--- esc_telemetry/esc_telemetry.ino
+++ esc_telemetry/esc_telemetry.ino
@@ -7,11 +7,6 @@
 #include "gooseka_structs.h"
 #include "gooseka_defs.h"
 
-typedef struct {
-    uint8_t received_bytes;
-    uint8_t buffer[SERIAL_BUFFER_SIZE];
-} serial_buffer_t;
-
 HardwareSerial LEFT_ESC_serial(1);
 HardwareSerial RIGHT_ESC_serial(2);
 
@@ -30,7 +25,10 @@ uint8_t RIGHT_duty = 0;
 
 ESC_control_t control;
 
-void receive_task(void* param) {
+// CPU #1
+// 0. Read incoming LoRa message
+// 1. Set LEFT & RIGHT ESC duty cycle
+void LoRa_receive_task(void* param) {
     uint8_t LoRa_buffer[sizeof(ESC_control_t)];
     uint8_t index;
 
@@ -49,7 +47,63 @@ void receive_task(void* param) {
     vTaskDelete(NULL);
 }
 
+// CPU #0
+// 0. Set LEFT & RIGHT ESC PWM
+// 1. Read LEFT ESC telemetry
+// 2. Read RIGHT ESC telemetry
+// 3. If both telemetries are complete, send LoRa message
+void ESC_control_task(void* param) {
+    while (1) {
+        /************** REPLACE ***********************/
+        // char command = '0';
+        // if(Serial.available()) {
+        //     command = Serial.read();
+        //     if(command == '+') {
+        //         LEFT_duty = min(LEFT_duty + 32, 255);
+        //         RIGHT_duty = min(RIGHT_duty + 32, 255);
+        //     } else if (command == '-') {
+        //         LEFT_duty = max(LEFT_duty - 32, 0);
+        //         RIGHT_duty = max(RIGHT_duty - 32, 0);
+        //     }
+        // }
+        /************** REPLACE END ********************/
+        LEFT_duty = control.left.duty;
+        RIGHT_duty = control.right.duty;
+        
+        LEFT_ESC_servo.write(map(LEFT_duty,0,255,0,180));
+        RIGHT_ESC_servo.write(map(RIGHT_duty,0,255,0,180));
+        
+        if(LEFT_ESC_serial.available()) {
+            if(read_telemetry(&LEFT_ESC_serial, &LEFT_serial_buffer, &(telemetry.left))) {
+                telemetry.left.duty = LEFT_duty;
+                LEFT_telemetry_complete = true;
+            }
+        }
+
+        if(RIGHT_ESC_serial.available()) {
+            if(read_telemetry(&RIGHT_ESC_serial, &RIGHT_serial_buffer, &(telemetry.right))) {
+                telemetry.right.duty = RIGHT_duty;
+                RIGHT_telemetry_complete = true;
+            }
+        }
+
+        if(LEFT_telemetry_complete && RIGHT_telemetry_complete) {
+            LEFT_telemetry_complete = false;
+            RIGHT_telemetry_complete = false;
+            print_telemetry(&Serial, &telemetry);
+            LoRa.beginPacket();
+            LoRa.write((uint8_t *)&telemetry, sizeof(ESC_telemetry_t)); 
+            LoRa.endPacket();
+        }
+        vTaskDelay(1); // Without this line watchdog resets the board
+    }
+    vTaskDelete(NULL);
+}
+
 void setup() {
+    // Initialize structs and arrays
+    // TO DO
+
     // Console output
     Serial.begin(115200);
 
@@ -80,80 +134,12 @@ void setup() {
     }
     LoRa.setSyncWord(LORA_SYNCWORD);
 
+    // Start ESC control task
+    xTaskCreatePinnedToCore(ESC_control_task, "ESC_controller", 10000, NULL, 1, NULL, 0);
     // Start LoRa receiver task
-    xTaskCreatePinnedToCore(receive_task, "lora_receiver", 10000, NULL, 1, NULL, 1);
-}
-
-bool read_telemetry(HardwareSerial* serial, serial_buffer_t* serial_buffer, ESC_oneside_telemetry_t* telemetry) {
-    serial_buffer->buffer[serial_buffer->received_bytes] = serial->read();
-    serial_buffer->received_bytes++;
-
-    if(serial_buffer->received_bytes > 9){ // transmission completed
-        serial_buffer->received_bytes = 0;
-        uint8_t crc8 = get_crc8(serial_buffer->buffer, 9); // get the 8 bit CRC
-        
-        if(crc8 != serial_buffer->buffer[9]) { // CRC ERROR                
-            // Empty Rx Serial of garbage telemtry
-            while(serial->available()) {
-                serial->read();
-            }
-            return false; // transmission failure 
-        }
-
-        // compute the received values
-        telemetry->timestamp = millis();
-        telemetry->temperature = serial_buffer->buffer[0]; // temperature
-        telemetry->voltage = (serial_buffer->buffer[1]<<8) | serial_buffer->buffer[2]; // voltage
-        telemetry->current = (serial_buffer->buffer[3]<<8) | serial_buffer->buffer[4]; // Current
-        telemetry->power = (serial_buffer->buffer[5]<<8) | serial_buffer->buffer[6]; // used mA/h
-        telemetry->erpm = (serial_buffer->buffer[7]<<8) | serial_buffer->buffer[8]; // eRpM *100
-
-        return true;
-    }
-    return false;
+    xTaskCreatePinnedToCore(LoRa_receive_task, "lora_receiver", 10000, NULL, 1, NULL, 1);
 }
 
 void loop() {
-    // TODO RECEIVE COMMANDS VIA LORA
-    /************** REPLACE ***********************/
-    // char command = '0';
-    // if(Serial.available()) {
-    //     command = Serial.read();
-    //     if(command == '+') {
-    //         LEFT_duty = min(LEFT_duty + 32, 255);
-    //         RIGHT_duty = min(RIGHT_duty + 32, 255);
-    //     } else if (command == '-') {
-    //         LEFT_duty = max(LEFT_duty - 32, 0);
-    //         RIGHT_duty = max(RIGHT_duty - 32, 0);
-    //     }
-    // }
-    /************** REPLACE END ********************/
-    LEFT_duty = control.left.duty;
-    RIGHT_duty = control.right.duty;
-    
-    LEFT_ESC_servo.write(map(LEFT_duty,0,255,0,180));
-    RIGHT_ESC_servo.write(map(RIGHT_duty,0,255,0,180));
-    
-    if(LEFT_ESC_serial.available()){
-       if(read_telemetry(&LEFT_ESC_serial, &LEFT_serial_buffer, &(telemetry.left))) {
-           telemetry.left.duty = LEFT_duty;
-           LEFT_telemetry_complete = true;
-       }
-    }
-
-    if(RIGHT_ESC_serial.available()){
-        if(read_telemetry(&RIGHT_ESC_serial, &RIGHT_serial_buffer, &(telemetry.right))) {
-           telemetry.right.duty = RIGHT_duty;
-           RIGHT_telemetry_complete = true;
-       }
-    }
-
-    if(LEFT_telemetry_complete && RIGHT_telemetry_complete) {
-        LEFT_telemetry_complete = false;
-        RIGHT_telemetry_complete = false;
-        print_telemetry(&Serial, &telemetry);
-        LoRa.beginPacket();
-        LoRa.write((uint8_t *)&telemetry, sizeof(ESC_telemetry_t)); 
-        LoRa.endPacket();
-    }
+    delay(2147483647L); // Delay forever
 }
--- esc_telemetry/gooseka_helpers.h
+++ esc_telemetry/gooseka_helpers.h
@@ -5,6 +5,12 @@
 #include "gooseka_structs.h"
 #include "gooseka_defs.h"
 
+typedef struct {
+    uint8_t received_bytes;
+    uint8_t buffer[SERIAL_BUFFER_SIZE];
+} serial_buffer_t;
+
+
 uint8_t update_crc8(uint8_t crc, uint8_t crc_seed) {
     uint8_t crc_u, i;
     crc_u = crc;
@@ -45,6 +51,35 @@ void print_telemetry(HardwareSerial *serial, ESC_telemetry_t *telemetry) {
     serial->print(telemetry->right.power); 
     serial->print(","); 
     serial->println(telemetry->right.erpm * 100 / (MOTOR_POLES / 2)); 
+}
+
+bool read_telemetry(HardwareSerial* serial, serial_buffer_t* serial_buffer, ESC_oneside_telemetry_t* telemetry) {
+    serial_buffer->buffer[serial_buffer->received_bytes] = serial->read();
+    serial_buffer->received_bytes++;
+
+    if(serial_buffer->received_bytes > 9){ // transmission completed
+        serial_buffer->received_bytes = 0;
+        uint8_t crc8 = get_crc8(serial_buffer->buffer, 9); // get the 8 bit CRC
+        
+        if(crc8 != serial_buffer->buffer[9]) { // CRC ERROR                
+            // Empty Rx Serial of garbage telemtry
+            while(serial->available()) {
+                serial->read();
+            }
+            return false; // transmission failure 
+        }
+
+        // compute the received values
+        telemetry->timestamp = millis();
+        telemetry->temperature = serial_buffer->buffer[0]; // temperature
+        telemetry->voltage = (serial_buffer->buffer[1]<<8) | serial_buffer->buffer[2]; // voltage
+        telemetry->current = (serial_buffer->buffer[3]<<8) | serial_buffer->buffer[4]; // Current
+        telemetry->power = (serial_buffer->buffer[5]<<8) | serial_buffer->buffer[6]; // used mA/h
+        telemetry->erpm = (serial_buffer->buffer[7]<<8) | serial_buffer->buffer[8]; // eRpM *100
+
+        return true;
+    }
+    return false;
 }
 
 #endif /* GOOSEKA_HELPERS_H */
