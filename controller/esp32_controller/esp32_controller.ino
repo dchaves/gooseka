529b8cfca9b64915bdcc95f2ca1edd5e838c17f6
David Chaves <dchaves@gradiant.org>
2020-02-06 13:28:42 +0100

    Implementacion de gateway LoRa

--- controller/esp32_controller/esp32_controller.ino
+++ controller/esp32_controller/esp32_controller.ino
@@ -4,23 +4,41 @@
 #include "esp32_controller_defs.h"
 #include "gooseka_structs.h"
 
-ESC_control_t control;
+#define STATE_SOF_1 0x00
+#define STATE_SOF_2 0x01
+#define STATE_FRAME 0x02
 
-ESC_telemetry_t telemetry;
+#define SOF_1 0xDE
+#define SOF_2 0xAD
+
+typedef struct {
+    uint8_t index;
+    uint8_t buffer[sizeof(ESC_control_t)];
+} USB_buffer_t;
+
+typedef struct {
+    uint8_t index;
+    uint8_t buffer[sizeof(ESC_telemetry_t)];
+} LoRa_buffer_t;
+
+USB_buffer_t USB_buffer;
+LoRa_buffer_t LoRa_buffer;
+
+uint8_t state;
 
 void LoRa_receive_task(void* param) {
     while(1) {
         int packetSize = LoRa.parsePacket();
         if (packetSize) {
-            // Serial.print("Received byte 0x");
-            // while (LoRa.available()) {
-            //     uint8_t data = LoRa.read();
-            //     Serial.print(data, HEX);
-            // }
-            // Serial.print(" with RSSI ");
-            // Serial.println(LoRa.packetRssi());
-            // Cast packet to struct
+            LoRa_buffer.index = 0;
+            while (LoRa.available()) {
+                LoRa_buffer.buffer[LoRa_buffer.index] = LoRa.read();
+                LoRa_buffer.index++;
+            }
             // Send packet via USB
+            Serial.write(SOF_1);
+            Serial.write(SOF_2);
+            Serial.write(LoRa_buffer.buffer, sizeof(ESC_telemetry_t));
         }
         vTaskDelay(1); // Without this line watchdog resets the board
     }     
@@ -29,19 +47,37 @@ void LoRa_receive_task(void* param) {
 
 void USB_receive_task(void* param) {
     while(1){
-        // Read commands from USB
-        // Do Stuff
         if (Serial.available() > 0) {
             // Receive USB char
-            // uint8_t incomingByte = Serial.read();
-            // Serial.print("Sent byte 0x");
-            // Serial.println(incomingByte, HEX);
-
-            // IF received a complete msg
-            // Send LoRa packet to receiver
-            // LoRa.beginPacket();
-            // LoRa.write(&incomingByte, 1); // MAX 255 bytes
-            // LoRa.endPacket();
+            uint8_t incomingByte = Serial.read();
+            switch(state) {
+                case STATE_SOF_1:
+                    if(incomingByte == SOF_1) {
+                        state = STATE_SOF_2;
+                    }
+                break;
+                case STATE_SOF_2:
+                    if(incomingByte == SOF_2) {
+                        state = STATE_FRAME;
+                        USB_buffer.index = 0;
+                    } else {
+                        sate = STATE_SOF_1;
+                    }
+                break;
+                case STATE_FRAME:
+                    if (USB_buffer.index < sizeof(ESC_control_t)) {
+                        USB_buffer.buffer[USB_buffer.index] = incomingByte;
+                        USB_buffer.index++;
+                    } else {
+                        state = STATE_SOF_1;
+                        LoRa.beginPacket();
+                        LoRa.write(USB_buffer.buffer, sizeof(ESC_control_t)); // MAX 255 bytes
+                        LoRa.endPacket();
+                    }
+                break;
+                default:
+                break;
+            }
         }
         vTaskDelay(1); // Without this line watchdog resets the board
     }
@@ -51,6 +87,9 @@ void USB_receive_task(void* param) {
 void setup() {
     // Initialize structs and arrays
     memset(&telemetry,0,sizeof(ESC_telemetry_t));
+    state = STATE_SOF_1;
+    memset(&USB_buffer, 0, sizeof(USB_buffer_t));
+    memset(&LoRa_buffer, 0, sizeof(LoRa_buffer_t));
 
     // USB output
     Serial.begin(SERIAL_BAUDRATE);
