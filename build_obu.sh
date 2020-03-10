58d40481ca70412089ba9197dd49e14cdc51269e
David Chaves <dchavesd@gmail.com>
2020-03-10 17:35:36 +0100

    Velocidad serie reducida a 115200 baudios hasta nuevas pruebas. Eliminado bug al seleccionar modulo de control. El MQTT se conecta si esta disponible, se ignora en caso contrario. Reorganizacion de scripts.

--- build_obu.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd arduino-cli-esp32
-docker build --tag arduino-cli-esp32 .
-cd ..
-docker run -it -v $(pwd)/gooseka-obu:/github/workspace arduino-cli-esp32 compile gooseka esp32:esp32:esp32doit-devkit-v1 ESP32Servo LoRa
--- flash_rsu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DEV=/dev/ttyUSB0
-
-cd arduino-cli-esp32
-docker build --tag arduino-cli-esp32 .
-cd ..
-docker run -it --device=$DEV -v $(pwd)/gooseka-rsu:/github/workspace arduino-cli-esp32 upload esp32_gateway esp32:esp32:esp32doit-devkit-v1 $DEV
