7ddb98a151d54b083926497323653786ba00f403
David Chaves <dchavesd@gmail.com>
2020-03-11 09:07:23 +0100

    Añadido puerto serie como parametro opcional a los scripts de flash

--- scripts/flash_obu.sh
+++ scripts/flash_obu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DEV=/dev/ttyUSB1
+DEV="${1:-/dev/ttyUSB0}"
 
 docker build --tag arduino-cli-esp32 arduino-cli-esp32
 docker run -it --device=$DEV -v $(pwd)/gooseka-obu:/github/workspace arduino-cli-esp32 upload gooseka esp32:esp32:esp32doit-devkit-v1 $DEV
--- scripts/flash_rsu.sh
+++ scripts/flash_rsu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DEV=/dev/ttyUSB0
+DEV="${1:-/dev/ttyUSB0}"
 
 docker build --tag arduino-cli-esp32 arduino-cli-esp32
 docker run -it --device=$DEV -v $(pwd)/gooseka-rsu:/github/workspace arduino-cli-esp32 upload esp32_gateway esp32:esp32:esp32doit-devkit-v1 $DEV
