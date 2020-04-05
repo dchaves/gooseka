74d25bb50a01bb455cd362ccac067373b8de8469
David Chaves <dchavesd@gmail.com>
2020-04-05 17:58:30 +0200

    Updated manual and machote commands

--- scripts/start_controller.sh
+++ scripts/start_controller.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker network create --driver bridge gooseka || true
-docker build --tag gooseka_controller gooseka-controller && docker run -it --privileged --network gooseka -v /dev/input:/dev/input --device=/dev/ttyUSB0 --env GOOSEKA='BENCHY' gooseka_controller
+CONTROLLER="${1:-BENCHY}"
+docker build --tag gooseka_controller gooseka-controller && docker run -it --privileged --network gooseka -v /dev/input:/dev/input --device=/dev/ttyUSB0 --env GOOSEKA=$CONTROLLER gooseka_controller
